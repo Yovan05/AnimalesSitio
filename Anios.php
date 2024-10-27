95fb3b3471ba3dc4711d4ec3ff9d4558dd83d061
Yovan Rodriguez <yovan.rodriguez244601@potros.itson.edu.mx>
2024-10-26 18:33:09 -0700

    Se añadio la funcion de calcular cuanto falta para el proximo año bisciesto

--- Anios.php
+++ Anios.php
@@ -2,15 +2,19 @@
 
 <script src="scripts/anios.js"></script>    
     <div class="container">
-        <h1>Lista de años visciestos</h1>
+        <h1>Lista de años Bisciestos</h1>
         <h2>Otra cosa</h2>
 
         <input type="number" id="anioInicio"></input>
         <input type="number" id="anioFinal"></input>
 
         <button onclick="anios()">OK</button>
+        <button onclick="clean()">Limpiar</button>
         <div id= "aniosV"></div>
 
+        <h1>Proximo año bisciesto</h1>
+        <button onclick="diasFaltantes()">Calcular</button>
+        <div id="faltantes"></div>
 
        
     </div>
--- scripts/anios.js
+++ scripts/anios.js
@@ -4,13 +4,43 @@ function anios(){
     var anioFinal= document.getElementById("anioFinal").value;
     var div_aniosV = document.getElementById("aniosV");
     div_aniosV.innerHTML ='<p></p>';
-    for(let i = anioInicial; i <= anioFinal; i++ ){
-        if(i%4==0){
-            div_aniosV.innerHTML +='<p class="visciesto">' + i + '</p>'; 
-        }else{
-            div_aniosV.innerHTML +='<p>' + i + '</p>';
+    if(anioInicial < anioFinal){
+        for(let i = anioInicial; i <= anioFinal; i++ ){
+            if(i%4==0){
+                div_aniosV.innerHTML +='<p class="bisciesto">' + i + '</p>'; 
+            }else{
+                div_aniosV.innerHTML +='<p>' + i + '</p>';
+            }
+            
         }
-        
+    }else{
+        alert("El año de incio debe ser menor al año final")
     }
+}
 
+function clean(){
+    var anioInicial= document.getElementById("anioInicio").value ="";
+    var anioFinal= document.getElementById("anioFinal").value = "";
+    var div_aniosV = document.getElementById("aniosV");
+    div_aniosV.innerHTML ='<p></p>';
+}
+
+function diasFaltantes(){
+    var today = new Date();
+    var year = today.getFullYear();
+    var div_faltantes = document.getElementById("faltantes");
+    if(year%4==0){
+        var yearNextB = year+4;
+    }else{
+        while((year+1)%4 != 0){
+            year++;
+        }
+        yearNextB = year+1;
+    }
+    var bisDate = new Date(`${yearNextB}-01-01`);
+    var diffDays = bisDate-today;
+    var daysLeft = Math.ceil(diffDays/(1000*60*60*24));
+    var yearLeft = Math.floor(daysLeft/365);
+    div_faltantes.innerHTML = '<h2> El proximo año bisciesto es en: '+yearNextB+'</h2>';
+    div_faltantes.innerHTML += '<h2> Faltan: '+yearLeft+' años con '+(daysLeft-(yearLeft*365))+ ' dias</h2>';
 }
